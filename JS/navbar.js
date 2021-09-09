8b06528267490fa5479cd2d425ba36ec65b1913b
Dylan_ <dylan.weisse@hotmail.com>
2021-09-09 16:56:09 +0200

    Update - 09/09/2021

---
+++ JS/navbar.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+      if ($(document).scrollTop() > 950) {
+        $(".nav").css("background-color", "#7451eb");
+        $(".nav-menu-link").css("color", "white");
+      } else {
+        $(".nav").css("background-color", "white");
+        $(".nav").css("transition", "0.3s");
+        $(".nav-menu-link").css("color", "#7451eb");
+        $(".nav-menu-link").css("transition", "0.1s");
+      }
+    });
+  });
+  
+  function toggler() {
+    let mi = document.querySelector('#hamburger-icon').classList.toggle('active');
+    let sn = document.querySelector('#side-nav');
+  
+    return mi ? sn.style.left = '0' : sn.style.left = '-250px';
+  }
+  
+  
