d3f6c92a77f49f17dca840cffef25581f7ad5417
Dylan_ <dylan.w@dylan-weisse.com>
2021-10-06 11:47:59 +0200

    UpDate - 06/10/2021 - v0.3.2

---
+++ JS/carrousel.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:5,
+            nav:true,
+            loop:false
+        }
+    }
+})
