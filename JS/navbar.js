5f88cfde77be3d096c438333fe0eccdca9a4700a
Dylan_ <dylan.weisse@hotmail.com>
2022-01-05 19:03:04 +0100

    Update - Projet

--- JS/navbar.js
+++ JS/navbar.js
@@ -1,17 +1,3 @@
-// $(document).ready(function () {
-//     $(window).scroll(function () {
-//       if ($(document).scrollTop() > 800) {
-//         $(".nav").css("background-color", "#7451eb");
-//         $(".nav-menu-link").css("color", "white");
-//       } else {
-//         $(".nav").css("background-color", "transparant");
-//         $(".nav").css("transition", "0.3s");
-//         $(".nav-menu-link").css("color", "#fff");
-//         $(".nav-menu-link").css("transition", "0.1s");
-//       }
-//     });
-//   });
-  
   function toggler() {
     let mi = document.querySelector('#hamburger-icon').classList.toggle('active');
     let sn = document.querySelector('#side-nav');
@@ -19,4 +5,5 @@
     return mi ? sn.style.left = '0' : sn.style.left = '-250px';
   }
   
-  
+
+ 
